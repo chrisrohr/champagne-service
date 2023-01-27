89e6653a0348c22cccdee8bcb9914d295f435c61
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2023-01-26 22:41:02 -0500

    Add test for missing coverage

--- src/main/java/org/kiwiproject/champagne/resource/AuditableResource.java
+++ src/main/java/org/kiwiproject/champagne/resource/AuditableResource.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class AuditableResource {
     
-    private AuditRecordDao auditRecordDao;
+    private final AuditRecordDao auditRecordDao;
 
     protected AuditableResource(AuditRecordDao auditRecordDao) {
         this.auditRecordDao = auditRecordDao;
