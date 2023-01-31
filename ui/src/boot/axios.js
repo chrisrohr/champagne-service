507660cb3b68159ddffe295e18f0fb23ec330d28
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2023-01-31 11:47:06 -0500

    Sonar fix

--- ui/src/boot/axios.js
+++ ui/src/boot/axios.js
@@ -1,6 +1,5 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
-import router from 'src/router'
 import { notifyError } from 'src/utils/alerts'
 
 // Be careful when using SSR for cross-request state pollution
