74ae35d51c2cc12363e73b83a59d3cd57f30250b
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2023-02-08 18:23:30 +0000

    Add models for Host and Components
    Closes #110

---
+++ service/src/main/java/org/kiwiproject/champagne/model/Host.java
@@ -0,0 +1,44 @@
+package org.kiwiproject.champagne.model;
+
+import java.time.Instant;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Representation of a host used to deploy components. A Host could be created and stored in the database or 
+ * dynamically pulls from a cloud provider like AWS.
+ */
+@Builder
+@Getter
+public class Host {
+
+    public enum Source {
+        CHAMPAGNE, AWS
+    }
+    
+    Long id;
+    Instant createdAt;
+    Instant updatedAt;
+
+    /**
+     * The id of the {@link DeploymentEnvironment} that this host lives in.
+     */
+    Long environmentId;
+
+    /**
+     * The hostname of the host.
+     */
+    String hostname;
+
+    /**
+     * List of tags used to link up components to hosts dynamically.
+     */
+    List<String> tags;
+
+    /**
+     * The source of truth for the definition of this host, whether it is stored in Champagne or pulled dynamically from a cloud.
+     */
+    Source source;
+}
