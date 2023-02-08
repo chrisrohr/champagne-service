764d36f12c0aac4802da29ab7a4205b2f16dcc14
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2023-02-08 14:53:44 -0500

    Add models for Host and Components (#112)
    Closes #110

---
+++ service/src/main/java/org/kiwiproject/champagne/model/Component.java
@@ -0,0 +1,29 @@
+package org.kiwiproject.champagne.model;
+
+import java.time.Instant;
+
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * Representation of a deployable component in the system.
+ */
+@Builder
+@Value
+public class Component {
+    
+    Long id;
+    Instant createdAt;
+    Instant updatedAt;
+    
+    /**
+     * The name of the component
+     */
+    String componentName;
+
+    /**
+     * A tag for the component used to link up with {@link Host} instances for deployments
+     */
+    String tag;
+
+}
